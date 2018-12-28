7477a3f8d89bc92dee90fdbd0ab98a83e277c5ab
afeiship <afeiship@gmail.com>
2018-12-28 23:55:00 +0800

    feat: init

---
+++ README.md
@@ -0,0 +1,12 @@
+# next-stub-singleton
+> Stub code for singleton
+
+## install:
+```bash
+npm install -S afeiship/next-stub-singleton --registry=https://registry.npm.taobao.org
+```
+
+## usage:
+```js
+//DOCS here!
+```
---
+++ src/next-stub-singleton.js
@@ -0,0 +1,20 @@
+(function() {
+  var global = global || this || window || Function('return this')();
+  var nx = global.nx || require('next-js-core2');
+
+  nx.stubSingleton = function() {
+    return {
+      instance: null,
+      getInstance: function() {
+        if (!this.instance) {
+          this.instance = new this();
+        }
+        return this.instance;
+      }
+    };
+  };
+
+  if (typeof module !== 'undefined' && module.exports) {
+    module.exports = nx.stubSingleton;
+  }
+})();
