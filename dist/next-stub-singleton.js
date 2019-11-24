0b6119d0839df91f185bf569f83f559b50995349
afeiship <1290657123@qq.com>
2019-11-24 16:11:18 +0800

    fix: build @scope pkg

--- dist/next-stub-singleton.js
+++ dist/next-stub-singleton.js
@@ -2,13 +2,13 @@
  * name: @feizheng/next-stub-singleton
  * url: https://github.com/afeiship/next-stub-singleton
  * version: 1.0.0
- * date: 2019-11-24T08:09:56.381Z
+ * date: 2019-11-24T08:11:11.475Z
  * license: MIT
  */
 
 (function() {
   var global = global || this || window || Function('return this')();
-  var nx = global.nx || require('next-js-core2');
+  var nx = global.nx || require('@feizheng/next-js-core2');
 
   nx.stubSingleton = function() {
     return {
